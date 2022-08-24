dfc49792e62cacebc206b73a889281cc4a9a5f7e
leafylemontree <sandlprogvideos@gmail.com>
2022-08-24 15:30:34 -0400

    Added missing directories

--- listener.py
+++ listener.py
@@ -1,69 +1,29 @@
-import subprocess
-import sys
-import os
+from src            import objects
+from src            import commands
+from src            import subprocess
+import asyncio
 from edamino import Bot, Context, Client, api
-from edamino.api import File
-from edamino.objects import UserProfile
-import json
-import threading
-import pathlib
-import socket
 
-path = pathlib.Path(__file__).parent.resolve()
-sys.path.insert(0, f'{path}/src/python')
-from commands import commands
-import bot_objects as bot_o
-
-time_ct = 300
+loop = False
 
 def main():
-
-    #server = bot_o.Server()
+    #server = objects.Server()
     bot = commands.login()
 
     @bot.event(message_types=api.MessageType.ALL, media_types=api.MediaType.ALL)
     async def on_message(ctx: Context):
-        
-        global time_ct
-        time_ct = 300
-        #try:
-        #    if ctx.msg.content.find("Good day. I want") != -1:
-        #        await ctx.client.delete_message(
-        #            chat_id=ctx.msg.threadId,
-        #            message_id=ctx.msg.messageId
-        #            )
-        #        await ctx.client.kick_from_chat(
-        #            ctx.msg.threadId,
-        #            ctx.msg.author.uid,
-        #            allow_rejoin=False
-        #        )
-        #        return
-        #    if ctx.msg.author.nickname.find("mambll_") != -1:
-        #        await ctx.client.kick_from_chat(
-        #            ctx.msg.threadId,
-        #            ctx.msg.author.uid,
-        #            allow_rejoin=False
-        #            )
-        #        return
-        #except Exception:
-        #    pass
-        reply = await commands.message(ctx);
-        return
+        global loop
+        if loop is False: loop = asyncio.get_event_loop()
+        subprocess.run(loop, ctx)
+        await commands.message(ctx);
 
-    #@bot.event()
-    #async def on_mention(ctx: Context):
-    #    return
+    bot.start();
+    #try:
+    #    bot.start();
+    #except:
+    #    print("Login failed!")
+    #    objects.botStats.write()
 
-    try:
-        bot.start();
-    except:
-        print("Login failed!")
-
-def listen():
-    global time_ct
-    time_ct -= 1
-    if time_ct == 0: sys.ext()
 
 if __name__ == "__main__":
 	main()
-
